640f715af820dff632bac817851838da231ce0fc
edgdougui_ayoub <edgdougui.ayoub@gmail.com>
2018-01-24 17:41:42 +0000

    show books with template thymeleaf

---
+++ src/main/java/com/springwebapp/first/controlers/BookControler.java
@@ -0,0 +1,20 @@
+package com.springwebapp.first.controlers;
+
+import com.springwebapp.first.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookControler {
+    private BookRepository bookRepository;
+
+    public BookControler(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books",bookRepository.findAll());
+        return "books";
+    }
+}
